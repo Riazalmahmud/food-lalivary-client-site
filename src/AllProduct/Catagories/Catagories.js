bde34368d151813bda890f7aed9b35d08b8378d5
Riazalmahmud <riazalmahmud041@gmail.com>
2022-02-08 19:31:34 +0600

    update

--- src/AllProduct/Catagories/Catagories.js
+++ src/AllProduct/Catagories/Catagories.js
@@ -1,26 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { Row } from 'react-bootstrap';
-import Category from '../Category/Category';
+import React, { useState, useEffect } from "react";
+import { Row } from "react-bootstrap";
+import useAuth from "../../context/AuthProvider/useAuth.js";
+import Category from "../Category/Category";
 const Catagories = () => {
-    const [categories, setCategories] = useState([]);
+  const { user } = useAuth();
+  const [categories, setCategories] = useState([]);
+  const shopCategory = { user: user.displayName, email: user.email };
+  const [addCart, setAddCart] = shopCategory;
 
-    useEffect(() => {
-        fetch('./Catagories.json')
-            .then(res => res.json())
-            .then(data => setCategories(data))
-    }, [])
-    return (
-        <div>
-            <Row xs={1} md={5} className="g-4">
-                {
-                    categories.map(category => <Category
-                        key={category.id}
-                        category={category}
-                    ></Category>)
-                }
-            </Row>
-        </div>
-    );
+  const handaleAddDelivary = (id) => {
+    const uri = `https://murmuring-cove-81107.herokuapp.com/products/${id}`;
+    fetch(uri)
+      .then((res) => res.json())
+      .then((data) => setAddCart(data));
+
+    const delivaryInfo = {
+      ...addCart,
+      displayName: user.displayName,
+      email: user.email,
+    };
+
+    fetch("https://murmuring-cove-81107.herokuapp.com/foodManage", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(delivaryInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert("added successfully");
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetch("https://murmuring-cove-81107.herokuapp.com/products")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div>
+      <Row xs={1} md={5} className="g-4">
+        {categories.map((category) => (
+          <Category
+            key={category.id}
+            category={category}
+            handaleAddDelivary={handaleAddDelivary}
+          ></Category>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default Catagories;
--- src/AllProduct/Category/Category.js
+++ src/AllProduct/Category/Category.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { Card, Col } from 'react-bootstrap';
-import './Category.css'
-const Category = ({ category }) => {
-    const { img } = category;
-    return (
-        <div>
-            <Col>
-                <Card className="category">
-                    <Card.Img variant="top" src={img} width={'150px'} />
-                </Card>
-            </Col>
-        </div>
-    );
+import React from "react";
+import { Card, Col } from "react-bootstrap";
+import "./Category.css";
+const Category = (props) => {
+  const { handaleAddDelivary } = props;
+  const { img, title, description, _id } = props.category;
+  console.log(props.category);
+  return (
+    <div>
+      <Col>
+        <Card className="category">
+          <div className="bg-rgba"></div>
+          <div className="product-info">
+            <h5 className="service-title fs-5">{title}</h5>
+            <p>{description}</p>
+            <button
+              type="button"
+              className="btn btn-danger fw-bold "
+              onClick={() => handaleAddDelivary(_id)}
+            >
+              Add to cart
+            </button>
+          </div>
+          <Card.Img variant="top" src={img} width={"150px"} />
+        </Card>
+      </Col>
+    </div>
+  );
 };
 
 export default Category;
